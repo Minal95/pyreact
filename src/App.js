63bd80d32359d3894d8b2f88bbec151ce6d33de9
Minal95 <minal.sharma09876@gmail.com>
2023-01-08 03:23:42 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Cards from "./components/Cards";
+import Navbar from './components/Navbar';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar />
+    <div className="d-flex justify-content-center pt-5">
+      
+    <Cards 
+    title="Javascript" 
+    src="m1.jpg"
+    prag="Javascript is used by programmers across the world to create dynamic and interactive web content like applications and browsers. JavaScript is so popular that it's the most used programming language in the world, used as a client-side programming language by 97.0% of all websites."
+    btn="Brendan Eich"
+    />
+    <Cards 
+    title="ReactJS" 
+    src="m2.jpg"
+    prag="React is a library for building composable user interfaces. It encourages the creation of reusable UI components that present data that changes over time. React is a powerful JavaScript UI library for creating modern applications."
+    btn="Jordan Walke"/>
+    <Cards 
+    title="NodeJS" 
+    src="m3.jpg"
+    prag="Node. js is a single-threaded, open-source, cross-platform runtime environment for building fast and scalable server-side and networking applications. It runs on the V8 JavaScript runtime engine, and it uses event-driven, non-blocking I/O architecture, which makes it efficient and suitable for real-time applications.
+    "
+    btn="Ryan Dahl"/>
     </div>
+    </>
   );
+
+  
 }
 
 export default App;
